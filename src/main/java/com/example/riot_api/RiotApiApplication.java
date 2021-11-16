360cbca0ed01777e00aac1dfed27b82d4fab4834
silasmj <74252789+silasmj@users.noreply.github.com>
2021-11-16 12:29:51 +0100

    Initial commit

---
+++ src/main/java/com/example/riot_api/RiotApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.riot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiotApiApplication.class, args);
+    }
+
+}
