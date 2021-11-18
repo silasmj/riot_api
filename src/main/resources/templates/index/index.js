220481df404fd0ca7adcaddc563b08f44df05ee7
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-11-18 11:06:51 +0100

    No apikey

--- src/main/resources/templates/index/index.js
+++ src/main/resources/templates/index/index.js
@@ -1,4 +1,4 @@
-fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-5c0a70dd-85ab-4cf3-8e0f-cbded1c25fe6")
+fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=APIKEYHERE")
     .then(response => response.json())
     .then(result => console.log(result))
 
