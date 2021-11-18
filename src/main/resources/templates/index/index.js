b867f681e04cbe1910314758f30d7f5717034f15
silasmj <74252789+silasmj@users.noreply.github.com>
2021-11-18 11:10:06 +0100

    search for summoner

--- src/main/resources/templates/index/index.js
+++ src/main/resources/templates/index/index.js
@@ -1,6 +1,11 @@
-fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-fe34a3bf-da62-41f5-ab3a-23165a896adb")
+let summonerSearch = "Doublelift";
+fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerSearch + "?")
     .then(response => response.json())
     .then(result => console.log(result))
 
-
-console.log("Hej")
+function inputFetchSummoner() {
+    summonerSearch = document.getElementById("search-for-summoner").value;
+    fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerSearch + "?")
+        .then(response => response.json())
+        .then(result => console.log(result))
+}
