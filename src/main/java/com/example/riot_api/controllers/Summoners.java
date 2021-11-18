e07f708bc9618a81927c43d7797b1e220fe09f1f
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-11-18 10:41:19 +0100

    Almost finished summoner controller

--- src/main/java/com/example/riot_api/controllers/Summoners.java
+++ src/main/java/com/example/riot_api/controllers/Summoners.java
@@ -3,8 +3,7 @@ package com.example.riot_api.controllers;
 import com.example.riot_api.models.Summoner;
 import com.example.riot_api.repositories.SummonerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,22 @@ public class Summoners {
     @GetMapping("/summoners")
     public List<Summoner> getSummoners(){
         return summoner.findAll();
+    }
+
+    @GetMapping("/summoners/{id}")
+    public Summoner getSummonerById(@PathVariable long id){
+        return summoner.getById(id);
+    }
+
+    @PostMapping("/summoners")
+    public Summoner addSummoner(@RequestBody Summoner newSummoner){
+        newSummoner.setId(null);
+        return summoner.save(newSummoner);
+    }
+
+    @DeleteMapping("/summoners/{id}")
+    public void deleteSummonerById(@PathVariable Long id) {
+        summoner.deleteById(id);
     }
 
 }
