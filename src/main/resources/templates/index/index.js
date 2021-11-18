ce6223e6fdebd6bd2fd1b5e1b55e35d0c089932c
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-11-18 11:05:51 +0100

    Added HTML Escape

--- src/main/resources/templates/index/index.js
+++ src/main/resources/templates/index/index.js
@@ -1,4 +1,4 @@
-fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-fe34a3bf-da62-41f5-ab3a-23165a896adb")
+fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-5c0a70dd-85ab-4cf3-8e0f-cbded1c25fe6")
     .then(response => response.json())
     .then(result => console.log(result))
 
