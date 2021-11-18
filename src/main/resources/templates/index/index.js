4c9083f1420589cbac672f67bde8c9c5e6b6fba1
silasmj <74252789+silasmj@users.noreply.github.com>
2021-11-18 11:11:15 +0100

    search for summoner

--- src/main/resources/templates/index/index.js
+++ src/main/resources/templates/index/index.js
@@ -1,11 +1,11 @@
 let summonerSearch = "Doublelift";
-fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerSearch + "?")
+fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerSearch + "?bbbb")
     .then(response => response.json())
     .then(result => console.log(result))
 
 function inputFetchSummoner() {
     summonerSearch = document.getElementById("search-for-summoner").value;
-    fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerSearch + "?")
+    fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerSearch + "?bbbb")
         .then(response => response.json())
         .then(result => console.log(result))
 }
